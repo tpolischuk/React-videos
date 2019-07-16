88605da945545cdf73fa888e12395e4b6c2fc0dd
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-07-15 21:10:42 -0700

    Bug fixes, completed video app

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,10 @@ class App extends React.Component {
         videos: [],
         selectedVideo: null
     };
+
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    };
     
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -18,7 +22,10 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
@@ -29,11 +36,19 @@ class App extends React.Component {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList 
-                    onVideoSelect={this.onVideoSelect} 
-                    videos={this.state.videos}
-                 />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList 
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,6 +5,7 @@ const VideoList = ({ videos, onVideoSelect }) => {
 
     const renderedList = videos.map((video) => {
         return <VideoItem 
+            key={video.id.videoId}
             onVideoSelect={onVideoSelect}
             video={video}
           />;
